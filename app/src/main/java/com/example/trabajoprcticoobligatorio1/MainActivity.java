9fe38b9dd453f7b7597887c571d398ab0d0d1578
Lorenzo-77 <lorenzo.geraldo.munoz@gmail.com>
2023-03-29 22:36:26 -0300

    primera entrega

---
+++ app/src/main/java/com/example/trabajoprcticoobligatorio1/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.trabajoprcticoobligatorio1;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.trabajoprcticoobligatorio1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MainActivityViewModel viewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+
+        viewModel.getResultado().observe(this, new Observer<Double>() {
+            @Override
+            public void onChanged(Double convert) {
+                binding.textResultado.setText(convert+"");
+            }
+        });
+
+        binding.radioDE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.radioED.setChecked(false);
+                binding.inputDolares.setEnabled(true);
+                binding.inputEuros.setEnabled(false);
+
+            }
+        });
+
+        binding.radioED.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.radioDE.setChecked(false);
+                binding.inputEuros.setEnabled(true);
+                binding.inputDolares.setEnabled(false);
+            }
+        });
+
+        binding.btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String option = "";
+                if(binding.radioDE.isChecked()){
+                    option = "D";
+                    viewModel.calcular(binding.inputDolares.getText().toString(), option);
+                }
+                if(binding.radioED.isChecked()){
+
+                    option = "E";
+                    viewModel.calcular(binding.inputEuros.getText().toString(), option);
+                }
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/trabajoprcticoobligatorio1/MainActivityViewModel.java
@@ -0,0 +1,57 @@
+package com.example.trabajoprcticoobligatorio1;
+
+import android.app.Application;
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    private Context context;
+    private MutableLiveData<Double> resultado = null;
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.context = application.getApplicationContext();
+    }
+
+    public LiveData<Double> getResultado() {
+        if (resultado == null) {
+            resultado = new MutableLiveData<>();
+        }
+        return resultado;
+    }
+
+
+    public void calcular(String num, String option){
+        try {
+            double dolar = 1;
+            double euro = 1.18;
+            double num1;
+            double res;
+
+            if(num.equals("")){
+                num1 = 0;
+            }else {
+                num1 = Double.parseDouble(num);
+                if(option.equals("D")){
+                    res = num1 * dolar;
+                    resultado.setValue(res);
+                }
+                if(option.equals("E")){
+                    res = num1 * euro;
+                    resultado.setValue(res);
+                }
+            }
+
+        }
+        catch (NumberFormatException e){
+            Toast.makeText(context, "error", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+}
